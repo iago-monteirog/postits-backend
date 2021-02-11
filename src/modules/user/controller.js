77587049cf0ca69e73c316f32b241a9809d52833
Iago Garcia <iagomgarcia9000@gmail.com>
2021-02-11 20:08:15 -0300

    created login and token

--- src/modules/user/controller.js
+++ src/modules/user/controller.js
@@ -1,14 +1,29 @@
-import { createUser } from './service'
-import { created, serverError, badRequest } from '../../utils/responses'
+import { createUser, loginService } from './service'
+import {
+  created, serverError, badRequest, ok,
+} from '../../utils/responses'
+
+const AUTHORIZATION_KEY = 'authorization'
 
 export const createAccount = async (event) => {
   try {
     const body = JSON.parse(event.body)
-    const user = await createUser(body)
-    return created(user)
+    const token = await createUser(body)
+    return created({ [AUTHORIZATION_KEY]: token })
   } catch (error) {
     if (error.code === 11000) return badRequest('email exists')
 
+    return serverError(error)
+  }
+}
+
+export const login = async (event) => {
+  try {
+    const body = JSON.parse(event.body)
+    const token = await loginService(body)
+    return ok({ [AUTHORIZATION_KEY]: token })
+  } catch (error) {
+    if (error.message === 'password_incorrect') return badRequest('password_incorrect')
     return serverError(error)
   }
 }
